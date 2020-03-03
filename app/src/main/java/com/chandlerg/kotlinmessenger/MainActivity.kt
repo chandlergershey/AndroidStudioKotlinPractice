c9fd57630c70e987da7bdc47a40824be27f0463b
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-03-03 13:34:16 -0500

    I think this may be the final push

--- app/src/main/java/com/chandlerg/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/chandlerg/kotlinmessenger/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,19 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("MainActivity","Email is: " + email)
             Log.d("MainActivity", "Password: $password")
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener{
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    // else if successful
+                    Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
+
+                }
+
+
+
+
         }
 
         already_have_account_text_view.setOnClickListener {
