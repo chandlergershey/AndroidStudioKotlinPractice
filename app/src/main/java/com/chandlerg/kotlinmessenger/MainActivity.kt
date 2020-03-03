b10fed38631df6496c20cd0c9ed27c50240cfcae
Chandler Gerstenslager <chandlergerstenslager@users.noreply.github.com>
2020-03-03 12:42:40 -0500

    Work up to video 1

--- app/src/main/java/com/chandlerg/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/chandlerg/kotlinmessenger/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.chandlerg.kotlinmessenger
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val register = register_button_register.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            Log.d("MainActivity","Email is: " + email)
+            Log.d("MainActivity", "Password: $password")
+        }
+
+        already_have_account_text_view.setOnClickListener {
+            Log.d("MainActivity", "Try to show login activity")
+
+            // launch the login activity with an intent
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
